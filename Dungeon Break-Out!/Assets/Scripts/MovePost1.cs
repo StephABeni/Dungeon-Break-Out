e856551d0a57b2ee56a5cd1691a64e96a973d52f
StephABeni <stephabenamati@gmail.com>
2022-03-04 21:22:52 -0600

    Build 3/4

--- Dungeon Break-Out!/Assets/Scripts/MovePost1.cs
+++ Dungeon Break-Out!/Assets/Scripts/MovePost1.cs
@@ -7,6 +7,7 @@ public class MovePost1 : MonoBehaviour
     private bool canInteract;
     private int currentSlot;
     public GameObject statuePuzzle;
+    public GameObject archesPuzzle;
 
     public bool IsSolved { get { return currentSlot == 2; } }
     private Vector3[] slots = { new Vector3(8.9999f, 1.2728f, -5.35f), new Vector3(8.9999f, 1.2728f, -5.85f), new Vector3(8.9999f, 1.2728f, -6.5f), new Vector3(8.9999f, 1.2728f, -7.0f) };
@@ -19,13 +20,10 @@ public class MovePost1 : MonoBehaviour
     {
         if (canInteract && InputManager.instance.ePressed)
         {
-            if (statuePuzzle.GetComponent<AddStatueToBox>().isComplete)
+            if (statuePuzzle.GetComponent<AddStatueToBox>().isComplete && archesPuzzle.GetComponent<archPuzzle>().IsSolved)
             {
                 Move();
-            }
-            else
-            {
-                // add something to say doesn't work
+                canInteract = false;
             }
         }
     }
@@ -44,8 +42,12 @@ public class MovePost1 : MonoBehaviour
     {
         if (other.tag == "Player")
         {
-            //if (itemInfo.ItemType == 5) return;
-            UIController.instance.ActivateDialog("[Press 'E'] Move Post");
+            if (!statuePuzzle.GetComponent<AddStatueToBox>().isComplete || !archesPuzzle.GetComponent<archPuzzle>().IsSolved) {
+                UIController.instance.ActivateDialog("It's locked in place. I need to solve another puzzle or two first.");
+            }
+            else {
+                UIController.instance.ActivateDialog("[Press 'E'] Move Post");
+            }
             canInteract = true;
         }
     }
